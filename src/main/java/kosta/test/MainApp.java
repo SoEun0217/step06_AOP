82cdb46f961e60a6c548368829c98bd4535596a8
kse02 <kse02@192.168.219.104>
2020-11-16 23:11:15 +0900

    1116 AOP 과제 완료

--- src/main/java/kosta/test/MainApp.java
+++ src/main/java/kosta/test/MainApp.java
@@ -3,11 +3,27 @@ package kosta.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import kosta.test.service.Player;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		ApplicationContext context = 
-				new ClassPathXmlApplicationContext();
+				new ClassPathXmlApplicationContext("kosta/test/applicationContext.xml");
+		Player tv = context.getBean("tvImpl",Player.class);
+		Player video = context.getBean("videoImpl",Player.class);
+		Player audio = context.getBean("audioImpl",Player.class);
+		tv.start(2);
+		tv.pause();
+		tv.stop();
+		System.out.println("----------------------");
+		audio.start(2);
+		audio.pause();
+		audio.stop();
+		System.out.println("----------------------");
+		video.start(2);
+		video.pause();
+		video.stop();
 	}
 
 }
--- src/main/java/kosta/test/advice/LogAdvice.java
+++ src/main/java/kosta/test/advice/LogAdvice.java
@@ -4,22 +4,28 @@ import java.io.File;
 import java.io.FileWriter;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class LogAdvice {
 	FileWriter fw = null;
+
 	public LogAdvice() {}
-	
-	public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
+
+	public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+		StopWatch sw = new StopWatch();
+		sw.start();
 		String methodName = joinPoint.getSignature().getName();
-		Object [] params = joinPoint.getArgs();
-		File file = new File("result/result.txt");
-		fw = new FileWriter(file,true);
-		fw.write(methodName+"호출");
-		
-		
+		Object[] params = joinPoint.getArgs();
+		File file = new File("result.txt");
+		fw = new FileWriter(file, true);
+		fw.write(methodName + "호출  /  인수 개수 : "+params.length+" / ");
+
 		Object obj = joinPoint.proceed();
-		
+		sw.stop();
+		fw.write("리턴 값 : "+obj +" / 총 실행 시간 : "+sw.getTotalTimeMillis()+"ms\n");
+		fw.flush();
+		fw.close();
 		return obj;
 	}
-	
+
 }
